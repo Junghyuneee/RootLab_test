ec035e0ac5552887cc28005078efc7998a80d011
한정현 <hanjunghyun95@gmail.com>
2024-11-15 17:31:55 +0900

    정규표현식

---
+++ Regular_Expressions/src/main/java/com/example/Regular_Expressions/RegularExpressionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Regular_Expressions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegularExpressionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegularExpressionsApplication.class, args);
+	}
+
+}
---
+++ Regular_Expressions/src/test/java/com/example/Regular_Expressions/RegularExpressionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Regular_Expressions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegularExpressionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
