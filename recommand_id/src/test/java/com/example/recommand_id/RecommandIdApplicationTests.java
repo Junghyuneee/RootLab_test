d016a60cb75cd9de9a59935c858ef2e0b66395b3
한정현 <hanjunghyun95@gmail.com>
2024-11-22 15:18:18 +0900

    신규 아이디 추천 로직

---
+++ recommand_id/src/test/java/com/example/recommand_id/RecommandIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recommand_id;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommandIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
